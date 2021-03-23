e2e64cb36696335dbecb0aea00d4818c1232e1a1
melihcan1376 <Melihcan1376@gmail.com>
2021-03-24 01:17:29 +0300

    first commit

---
+++ Music Player/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Music
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Stop = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.Transparent;
+            this.Stop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Stop.Image = ((System.Drawing.Image)(resources.GetObject("Stop.Image")));
+            this.Stop.Location = new System.Drawing.Point(150, 62);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(91, 87);
+            this.Stop.TabIndex = 2;
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.Color.Transparent;
+            this.Play.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Play.Image = ((System.Drawing.Image)(resources.GetObject("Play.Image")));
+            this.Play.Location = new System.Drawing.Point(43, 62);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(88, 87);
+            this.Play.TabIndex = 6;
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.Blue;
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 26);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(76, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Dosya Konumunu Girin:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(297, 168);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Stop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Player";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Music Player/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Music
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+
+        private void Play_Click(object sender, System.EventArgs e)
+        {
+            player.SoundLocation = textBox1.Text;
+            player.Play();
+        }
+
+        private void Stop_Click(object sender, System.EventArgs e)
+        {
+            player.Stop();
+        }
+    }
+}
